287eb27e8473c91883ee2620863d475213fc9aed
cqfkfc <chuaqinfang@gmail.com>
2021-08-31 12:27:02 +0800

    list component

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import ComponentOne from "./src/screens/ComponentOne";
 import HomeScreen from "./src/screens/HomeScreen";
+import ListComponent from "./src/screens/ListComponent";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    ComponentOne: ComponentOne
+    ComponentOne: ComponentOne,
+    ListComponent: ListComponent,
   },
   {
-    initialRouteName: "ComponentOne",
+    initialRouteName: "ListComponent",
     defaultNavigationOptions: {
       title: "App",
     },
---
+++ src/screens/ListComponent.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+// from ES6, Array.from is like range()
+const data = Array.from({ length: 20 }, (x, i) => ({
+  name: "Friend #" + Number(i + 1),
+  id: i,
+  age: Math.round(Math.random() * 30),
+}));
+
+const ListComponent = () => {
+  return (
+    <View>
+      <Text>List</Text>
+      <FlatList
+        // horizontal
+        data={data}
+        // the KEY helps react-native delete only that item
+        // instead of re-rendering everything
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator
+        renderItem={({ item }) => {
+          // console.log("item", item);
+          // it is confusing as it has a nested "item" key
+          // so, use ({item}) instead of (item)
+          return (
+            <Text style={styles.textStyle}>
+              {item.name} - Age {item.age}
+            </Text>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 20,
+    fontFamily: "Roboto",
+    padding: 5,
+    margin: 10,
+  },
+});
+
+export default ListComponent;
