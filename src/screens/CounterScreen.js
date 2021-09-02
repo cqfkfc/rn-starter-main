96fd325b153b09240d14e835ce38ee6c04bac3fc
cqfkfc <chuaqinfang@gmail.com>
2021-09-02 22:29:22 +0800

    Updated counterscreen

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, useS, Button, StyleSheet } from "react-native";
 
 const CounterScreen = () => {
+  const [count, setCount] = useState(0);
   return (
     <View>
       <Text>Counter Screen</Text>
+      <Button onPress={() => setCount(count + 1)} title="Increase" />
+      <Button onPress={() => setCount(count - 1)} title="Decrease" />
+      <Text style={styles.text}>{count.toString()}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 100,
+    alignSelf: "center",
+  },
+});
 
 export default CounterScreen;
