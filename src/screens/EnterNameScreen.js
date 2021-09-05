8b81ac853244459d00ba9df22e68c44628c4c268
cqfkfc <chuaqinfang@gmail.com>
2021-09-05 16:07:14 +0800

    update enternamescreen

--- src/screens/EnterNameScreen.js
+++ src/screens/EnterNameScreen.js
@@ -1,11 +1,33 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, SafeAreaView, TextInput, StyleSheet } from "react-native";
+
+// https://reactnative.dev/docs/textinput
+
 const EnterNameScreen = () => {
+  const [name, setName] = useState("Michael");
   return (
-    <View>
-      <Text>Enter Name Screen</Text>
-    </View>
+    <SafeAreaView>
+      <Text>Enter Name:</Text>
+      <TextInput
+        autoCapitalize={false}
+        autoCorrect={false}
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+      />
+      <Text>My Name is {name}.</Text>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    maxWidth: 400,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 
 export default EnterNameScreen;
